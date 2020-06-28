867e2a5abc20b4c1cadd8e847d399e26e92c34ed
Adam Faulconbridge <afaulconbridge@googlemail.com>
2020-06-28 20:44:02 +0100

    better precommit

--- README.md
+++ README.md
@@ -5,45 +5,53 @@ TODO
 development
 -----------
 
-Install using pip including development extras
+TL;DR
+```sh
+pip install -e .[dev] && pre-commit install
+```
 
+Install using pip including development extras
 ```sh
 pip install -e .[dev]
 ```
 
 Enable pre-commit hooks with:
-
 ```sh
 pre-commit install
 ```
 
-Freeze dependencies with:
+Run pre-commit hooks without committing:
+```sh
+pre-commit run --all-files
+```
 
+Note pre-commit is configured to use:
+ - seed-isort-config to better categorise third party imports
+ - isort to sort imports
+ - black to format code
+
+Freeze dependencies with:
 ```sh
 pip-compile
 ```
 
 Run tests with:
-
 ```sh
 pytest
 ```
 
 Test coverage with:
-
 ```sh
 coverage run --source=TODO -m pytest
 coverage report -m
 ```
 
 Type checking with:
-
 ```sh
 mypy .
 ```
 
 See dependencies with:
-
 ```sh
 pipdeptree
 ```
--- setup.py
+++ setup.py
@@ -1,24 +1,23 @@
 from setuptools import setup
 
 setup(
-    name='TODO',
-    version='1.0.0',
-    author='Adam Faulconbridge',
-    author_email='afaulconbridge@googlemail.com',
-    packages=['TODO'],
-    description='TODO.',
-    long_description=open('README.md').read(),
-    install_requires=[
-        ''
-    ],
+    name="TODO",
+    version="1.0.0",
+    author="Adam Faulconbridge",
+    author_email="afaulconbridge@googlemail.com",
+    packages=["TODO"],
+    description="TODO.",
+    long_description=open("README.md").read(),
+    install_requires=[""],
     extras_require={
-        'dev': [
-            'pytest-cov',
-            'flake8',
-            'pylint',
-            'pip-tools',
-            'pipdeptree',
-            'pre-commit'
+        "dev": [
+            "pytest-cov",
+            "flake8",
+            "black",
+            "pylint",
+            "pip-tools",
+            "pipdeptree",
+            "pre-commit",
         ],
-    }
+    },
 )
